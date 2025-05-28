f5fe5a4d52377379272017d1f925b756723b620e
= <=>
2025-05-28 17:52:24 -0300

    nao aguento maissss

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-const express =  require('express')
-const path = require('path')
-const fileupload = require('express-fileupload')
+const express = require('express');
+const path = require('path');
+const fileupload = require('express-fileupload');
 
-let initial_path = path.join(__dirname, "public")
+let initial_path = path.join(__dirname, "public");
 
-const app = express()
-app.use(express.static(initial_path))
-app.use(fileupload())
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
 
-app.get('/',  (req, res) => {
-    res.sendFile(path.join(initial_path, "index.html"))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "index.html"));
 })
 
 app.get('/editor', (req, res) => {
-    res.sendFile(path.join(initial_path, "editor.html"))
+    res.sendFile(path.join(initial_path, "editor.html"));
 })
 
-//upload link
+// upload link
 app.post('/upload', (req, res) => {
-    let file = req.files.image
-    let date = new Date()
+    let file = req.files.image;
+    let date = new Date();
     // image name
-    let imagename = date.getDate() + date.getTime() + file.name
+    let imagename = date.getDate() + date.getTime() + file.name;
     // image upload path
-    let path = 'public/uploads/' + imagename
+    let path = 'public/uploads/' + imagename;
 
     // create upload
     file.mv(path, (err, result) => {
         if(err){
-            throw err
+            throw err;
         } else{
             // our image upload path
             res.json(`uploads/${imagename}`)
@@ -37,12 +37,13 @@ app.post('/upload', (req, res) => {
 })
 
 app.get("/:blog", (req, res) => {
-    res.sendFile(path.join(initial_path, "blog.html"))
+    res.sendFile(path.join(initial_path, "blog.html"));
 })
 
 app.use((req, res) => {
-    res.json("404")
+    res.json("404");
 })
+
 app.listen("3000", () => {
-    console.log('listening')
+    console.log('listening......');
 })
