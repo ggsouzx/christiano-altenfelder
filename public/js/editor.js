d7d9e8977d67b9073a539d92e923541a797f0b20
= <=>
2025-05-28 23:35:12 -0300

    tentando ajeitar os posts

--- public/js/editor.js
+++ public/js/editor.js
@@ -23,7 +23,7 @@ const uploadImage = (uploadFile, uploadType) => {
         const formdata = new FormData()
         formdata.append('image', file)
 
-        fetch('/uploads', {
+        fetch('/upload', {
             method: 'post',
             body: formdata
         }).then(res => res.json())
